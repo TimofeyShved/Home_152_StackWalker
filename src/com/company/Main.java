e0deb3b81819973a07a8ad61364a53a115434ac3
TimofeyShved <timofeya123@inbox.ru>
2023-12-18 01:56:33 +0600

    StackWalker

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        one().forEach(x -> System.out.println(x));
+    }
+
+    private static List<String> one(){
+        return two();
+    }
+
+    private static List<String> two(){
+        // При помощи StackWalker и метода walk, можно проходить и получать весь стек вызовов
+        // создаёт map(коллекцию) и закидывает в неё frame(элементы) с названиями методов, до конечного вызова, а потом в главном классе выводит коллекцию на экран
+        return StackWalker.getInstance().walk(s -> s.map(frame -> frame.getClassName() +" : "+ frame.getMethodName()).collect(Collectors.toList()));
     }
 }
